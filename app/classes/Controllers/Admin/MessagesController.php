5ea9215e8edf9fe81e7c11acbf133332b785f9e6
Engaz01 <azakaite@github.com>
2020-12-09 16:27:51 +0200

    Add MessageTable to MessageController

--- app/classes/Controllers/Admin/MessagesController.php
+++ app/classes/Controllers/Admin/MessagesController.php
@@ -4,30 +4,25 @@
 namespace App\Controllers\Admin;
 
 
-use App\App;
 use App\Controllers\Base\AuthController;
 use App\Views\BasePage;
-use App\Views\Forms\Admin\DeleteForm;
-use App\Views\Tables\Admin\ProductsTable;
-use Core\Views\Form;
+use App\Views\Tables\Admin\MessagesTable;
 
 class MessagesController extends AuthController
 {
-    protected DeleteForm $form;
     protected BasePage $page;
 
     public function __construct()
     {
         parent::__construct();
-        $this->form = new deleteForm();
         $this->page = new BasePage([
-            'title' => 'Edit wishes',
+            'title' => 'Messages',
         ]);
     }
 
     public function messages(): ?string
     {
-        $table = new ProductsTable();
+        $table = new MessagesTable();
 
         $this->page->setContent($table->render());
 
--- app/classes/Views/Tables/Admin/MessagesTable.php
+++ app/classes/Views/Tables/Admin/MessagesTable.php
@@ -5,8 +5,6 @@ namespace App\Views\Tables\Admin;
 
 
 use App\App;
-use App\Views\Forms\Admin\DeleteForm;
-use Core\Views\Link;
 use Core\Views\Table;
 
 class MessagesTable extends Table
