e6f92af05b34a54c9f81e8f574903c6c550c7064
Engaz01 <azakaite@github.com>
2020-12-09 16:20:28 +0200

    Add to AboutController form validation

--- app/classes/Controllers/AboutController.php
+++ app/classes/Controllers/AboutController.php
@@ -4,6 +4,7 @@
 namespace App\Controllers;
 
 
+use App\App;
 use App\Views\BasePage;
 use App\Views\Forms\MessagesForm;
 use Core\View;
@@ -24,13 +25,21 @@ class AboutController
     function index(): ?string
     {
         // TODO: Implement index() method.
+        if ($this->form->validate()) {
+            $clean_inputs = $this->form->values();
+
+            App::$db->insertRow('messages', $clean_inputs);
+
+            $p = 'You sent a message';
+        }
 
         $content = new View([
             'title' => 'About Santa',
             'img' => 'https://images.theconversation.com/files/302306/original/file-20191118-169393-r78x4o.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop',
             'address' => 'Tähtikuja 1, 96930 Rovaniemi, Finland',
             'email' => 'joulupukinpaaposti@posti.fi',
-            'form' => $this->form->render()
+            'form' => $this->form->render(),
+            'message' => $p ?? null
         ]);
 
         $this->page->setContent($content->render(ROOT . '/app/templates/content/about.tpl.php'));
